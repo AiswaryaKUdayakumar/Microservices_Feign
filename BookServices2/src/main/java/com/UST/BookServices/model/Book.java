e8684d6880586298c3da6a79e2a565913f3de644
AiswaryaKUdayakumar <aiswaryakuday@gmail.com>
2023-05-18 17:01:45 +0530

    feign

---
+++ BookServices2/src/main/java/com/UST/BookServices/model/Book.java
@@ -0,0 +1,16 @@
+package com.UST.BookServices.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+        public Integer bookId;
+        public String bookName;
+        public Double bookCost;
+    }
+
